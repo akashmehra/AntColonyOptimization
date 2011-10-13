18806615ad7305f140e4ac8ddc1759e852453d62
Akash Mehra <mehra.akash@gmail.com>
2011-10-13 05:01:53 -0400

    functionality not complete, need to implement getNextVertex.

--- src/hps/aco/algorithm/AntColony.java
+++ src/hps/aco/algorithm/AntColony.java
@@ -2,16 +2,19 @@ package hps.aco.algorithm;
 
 public class AntColony 
 {
-	public AntColony(int NUM_CITIES, float alpha, float beta, float Q, float[][] edges)
+	public AntColony(int NUM_CITIES, float alpha, float beta, float Q, float evaporationQuotient, float[][] edges, int NUM_ANTS)
 	{
 		this.NUM_CITIES = NUM_CITIES;
 		this.alpha = alpha;
 		this.beta = beta;
 		this.Q = Q;
+		this.evaporationQuotient = evaporationQuotient;
 		this.initialPheromone = 1.0f/this.NUM_CITIES;
 		this.pheromones = new float[NUM_CITIES][NUM_CITIES];
 		this.invertedDistance = new float[NUM_CITIES][NUM_CITIES];
 		this.edges = edges;
+		this.NUM_ANTS = NUM_ANTS;
+		ants = new Ant[NUM_ANTS];
 		init();
 	}
 	
@@ -25,20 +28,52 @@ public class AntColony
 				invertedDistance[i][j] = 1.0f/edges[i][j];
 			}
 		}
+		for(int i=0;i<NUM_ANTS;++i)
+		{
+			ants[i] = new Ant();
+		}
+		
 	}
 	private float getPheromone(int i, int j)
 	{
 		return pheromones[i][j];
 	}
+	
+	private void setPheromone (int i, int j, float pheromone)
+	{
+		pheromones[i][j] = pheromone;
+	}
 	private int getNextVertex(int currentVertexIndex, double delta)
 	{
 		int index = 0;
+		//implement this, choosing next vertex is tricky, use heuristics here.
 		return index;
 	}
-	private double calculatePheromones()
+	
+	/*This is a global update.
+	 * Updates will be applied as follows:
+	 * T(u,v) = (1-evaporationQuotient)*T(u,v) + delT(u,v)
+	 * delT(u,v) = Q/ant.pathLength if ant uses edge (u,v) else 0.
+	 * We'll update the pheromone trail for every ant and not just the best ant, to explore as much as possible.
+	 * */
+	private void updatePheromone(Ant ant)
 	{
-		float pheromones = 0.0f;
-		return pheromones;
+		for(int i=0;i<NUM_CITIES-1;++i)
+		{
+			int u = ant.vertices[i];
+			int v = ant.vertices[i+1];
+			updatePheromoneTrail(ant, u, v);
+		}
+		int u = ant.vertices[NUM_CITIES-1];
+		int v = ant.vertices[0];
+		updatePheromoneTrail(ant, u, v);
+	}
+
+	private void updatePheromoneTrail(Ant ant, int u, int v)
+	{
+		float oldPheromone = getPheromone(u,v);
+		float updatedValue = (1-evaporationQuotient) * oldPheromone + Q/ant.pathLength;
+		setPheromone(u,v,updatedValue);
 	}
 	
 	/*The probability that an ant k chooses the edge (u,v) is expressed as:
@@ -90,9 +125,17 @@ public class AntColony
 	private float alpha = 0.0f;
 	private float beta = 0.0f;
 	private float Q = 0.0f;
+	private float evaporationQuotient = 0.0f;
 	private final int NUM_CITIES;
+	private final int NUM_ANTS;
 	private float pheromones[][];
 	private float invertedDistance[][];
 	private float edges[][];
+	private Ant[] ants;
+}
 
+class Ant
+{
+	int[] vertices;// vertices are added in the order they are visited.
+	int pathLength;
 }
